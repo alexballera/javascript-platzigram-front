5484d85d113e3bf6386c72ac6bd71adaeb9a7746
Alex Ballera <alexballera@gmail.com>
2016-12-28 15:20:01 -0400

    Update 2016-12-28T19:20:54.102Z

--- tasks/reload.js
+++
@@ -1,9 +0,0 @@
-import gulp from 'gulp'
-import browserSync from 'browser-sync'
-
-gulp.watch([
-  './src/**/*.html',
-  './src/styles/scss/**/*.scss',
-  './src/scripts/main.js',
-  './src/scripts/lib/*.js'
-]).on('change', browserSync.reload)
