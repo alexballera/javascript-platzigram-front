3bf654acf8fdb1f85c23e19c9226015a5871efab
Alex Ballera <alexballera@gmail.com>
2016-12-29 22:37:26 -0400

    Agregado page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,12 @@
 // import $ from 'jquery'
 // import materialize from '../../node_modules/materialize-css/dist/js/materialize.js'
-
+import views from './lib/views'
 (() => {
   'use strict'
 
   document.addEventListener('DOMContentLoaded', onDOMLoad)
 
   function onDOMLoad () {
+    views()
   }
 })()
